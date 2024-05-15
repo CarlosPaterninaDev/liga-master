91d612d70002592e2c2ac69cfe3b4642fc9f07ef
Jose Carlos Vasquez Cruz <josecarlosvasquez@gmail.com>
2024-05-14 23:23:42 -0500

    funcionalidad de match

--- src/app/modules/matches/matches.component.ts
+++ src/app/modules/matches/matches.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Match } from '../../models/match.model';
+import { MatchesService } from './match.service';
+import { CrearMatchComponent } from './crear-match/crear-match.component';
+import { SharedModule } from '../shared.module';
 
 @Component({
   selector: 'app-matches',
   standalone: true,
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './matches.component.html',
   styleUrl: './matches.component.scss'
 })
 export default class MatchesComponent {
+
+  displayedColumns: string[] = ['equipo1', 'golesEquipo1', 'equipo2', 'golesEquipo2'];
+  matches: Match[] ;
+
+  constructor(private dialog: MatDialog,
+    private matchService: MatchesService){
+      this.matches = this.matchService.getMacthes();
+    }
+
+  openAddMatchModal(): void{
+    const dialogRef = this.dialog.open(CrearMatchComponent,{
+      width: '250px'
+    });    
+
+    dialogRef.afterClosed().subscribe((result: any) => {
+      if(result){
+        this.matches = this.matchService.getMacthes();
+      }
+    });
+  }
+
 
 }
