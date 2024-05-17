05a0bff732891106b8ec27d4e9194c4e742b7f63
rmartinezb14 <rmartinez@jamar.com>
2024-05-17 17:30:20 -0500

    feature/filter-positions
    Se coloca filtro por nombre de equipos

--- src/app/modules/matches/standings/standings.component.ts
+++ src/app/modules/matches/standings/standings.component.ts
@@ -35,5 +35,8 @@ export class StandingsComponent implements OnInit {
     })
       
   }
-
+ applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
